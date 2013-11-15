72d5becb7b6683d694dcc6332164ee1a2ea00b82
Ash Blue <ash@blueashes.com>
2013-11-15 09:47:13 -0800

    Added a method to the articy_draft model that returns XML data

--- app/models/articy_draft.rb
+++ app/models/articy_draft.rb
@@ -1,7 +1,17 @@
 class ArticyDraft
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :ref, type: String
 
   validates_presence_of :ref, :message => 'You must provide a valid XML file'
+
+  def file_xml
+    loc = Rails.root + AppConstants::ARTICY_DRAFT_DIR + '/' + self.ref
+    file = File.open(loc)
+    xml_content = Nokogiri::XML(file)
+    file.close
+
+    xml_content
+  end
 end
