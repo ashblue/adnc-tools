2ce6bb19cd0c4cf8dfdac17f2f31a2888e1ecc6a
Ash Blue <ash@blueashes.com>
2013-11-15 23:14:52 -0800

    Added relationship data to Dialogue for Articy Draft and fixed a bug with xml file request method

--- app/models/articy_draft.rb
+++ app/models/articy_draft.rb
@@ -2,12 +2,14 @@ class ArticyDraft
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  has_one :dialogue
+
   field :ref, type: String
 
   validates_presence_of :ref, :message => 'You must provide a valid XML file'
 
   def file_xml
-    loc = Rails.root + AppConstants::ARTICY_DRAFT_DIR + '/' + self.ref
+    loc = Rails.root.to_s + '/' + AppConstants::ARTICY_DRAFT_DIR + '/' + self.ref
     file = File.open(loc)
     xml_content = Nokogiri::XML(file)
     file.close
